73bf205ad0415b28fce24dd0405c6baa2ed596e1
Tyler Conrad <tyler@essential-elements.net>
2013-07-19 17:02:19 -0500

    Fix.

--- kivy/uix/screenmanager.py
+++ kivy/uix/screenmanager.py
@@ -752,9 +752,9 @@ class ScreenManager(FloatLayout):
         except ValueError:
             return
 
-    def next(self):
-        ''' Py2K backwards compatability without six or other lib'''
-        return self.__next__()
+#    def next(self):
+#        ''' Py2K backwards compatability without six or other lib'''
+#        return self.__next__()
 
     def previous(self):
         '''Return the name of the previous screen from the screen list.
