cd277e8593a115b67bde6661b5e5b0a16342f766
Ryan Pessa <ryan@essential-elements.net>
2013-08-02 15:28:07 -0500

    * add cythonize undo, make executable, and use default arguments

--- cythonize.py
+++ cythonize.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 '''
 Usage: python cythonize.py path_to_setup.py py_file_path ...
 
@@ -7,31 +8,68 @@ python cythonize.py setup.py `cat cython_list
 from sys import argv
 from os import rename
 
-setup_py_path = argv[1]
-py_path_list = argv[2:]
+try:
+	undo_i = argv.index('--undo')
+	undo = True
+	argv = argv[:undo_i] + argv[undo_i + 1:]
+except:
+	undo = False
+
+setup_py_path = argv[1] if len(argv) > 1 else 'setup.py'
+py_path_list = argv[2:] if len(argv) > 2 else filter(None, file('cython_list').read().split('\n'))
 pyx_path_list = [path + 'x' for path in py_path_list]
 
 # read in setup.py
 with open(setup_py_path, 'r') as setup_py_file:
 	setup_py = setup_py_file.readlines()
 
-# rename py files to pyx
-for py_path, pyx_path in zip(py_path_list, pyx_path_list):
-	rename(py_path, pyx_path)
-
-	# add pyx files to sources list in setup.py
-	setup_py.insert(
-		setup_py.index('sources = {\n') + 1,
-		("    '{}': base_flags,\n").format(
-			'/'.join(pyx_path.split('/')[1:])))
+if undo:
+	for py_path, pyx_path in zip(py_path_list, pyx_path_list):
+		try:
+			rename(pyx_path, py_path)
+		except Exception, e:
+			print 'rename', pyx_path, 'to', py_path, '::'
+			print e
+		else:
+			line = "    '{}': base_flags,\n".format(
+						'/'.join(pyx_path.split('/')[1:]))
+			setup_py.remove(line)
+else:
+	# rename py files to pyx
+	for py_path, pyx_path in zip(py_path_list, pyx_path_list):
+		try:
+			rename(py_path, pyx_path)
+		except Exception, e:
+			print 'rename', py_path, 'to', pyx_path, '::'
+			print e
+		else:
+			# add pyx files to sources list in setup.py
+			setup_py.insert(
+				setup_py.index('sources = {\n') + 1,
+				("    '{}': base_flags,\n").format(
+					'/'.join(pyx_path.split('/')[1:])))
 
 # make core/text/__init__.py import from pyx file
 with open('kivy/core/text/__init__.py', 'r+') as py_file:
 	lines = py_file.readlines()
-	lines[
-		lines.index("DEFAULT_FONT = 'DroidSans'\n"):
-		lines.index("        doc='''(deprecated) Use text_size instead.''')\n") + 1
-	] = 'from ctext import *\n'
+	
+	try:
+		if undo:
+			i1 = lines.index("#DEFAULT_FONT = 'DroidSans'\n")
+			i2 = lines.index("#        doc='''(deprecated) Use text_size instead.''')\n") + 1
+			
+			for i in range(i1, i2):
+				lines[i] = lines[i][1:]
+			lines.remove('from ctext import *\n')
+		else:
+			i1 = lines.index("DEFAULT_FONT = 'DroidSans'\n")
+			i2 = lines.index("        doc='''(deprecated) Use text_size instead.''')\n") + 1
+			
+			for i in range(i1, i2):
+				lines[i] = '#' + lines[i]
+			lines.insert(i2, 'from ctext import *\n')
+	except Exception, e:
+		print e
 	
 	py_file.seek(0)
 	py_file.writelines(lines)
