a248677777b9b321f43bda51e1ebdfde69f2c9a6
Ryan Pessa <dkived@gmail.com>
2015-03-20 12:30:00 -0500

    remove pygame clipboard aliases if not needed

--- kivy/core/clipboard/clipboard_pygame.py
+++ kivy/core/clipboard/clipboard_pygame.py
@@ -50,7 +50,9 @@ class ClipboardPygame(ClipboardBase):
         if not self._types:
             self.init()
             types = pygame.scrap.get_types()
-            for mime, pygtype in self._aliases.items():
+            for mime, pygtype in self._aliases.items()[:]:
+                if mime in types:
+                    del self._aliases[mime]
                 if pygtype in types:
                     types.append(mime)
             self._types = types
